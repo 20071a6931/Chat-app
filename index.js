11277fdab753b0fd9ab5d4ae53f57ba03a56bfa0
Udaykiran <koradaudaykiran1406@gmail.com>
2023-05-07 11:34:27 +0530

    link changed to /

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/',(req,res)=>{
 const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin: "",
+        origin: "/",
         methods: ["GET","POST"],
     }
 }) 
